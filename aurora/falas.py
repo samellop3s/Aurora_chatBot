1a71729d2caca3e13fdf766180a69fd152e9fba8
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-09-22 14:05:46 -0300

    feat : criação do arquivos falas.py e gerando frases de conversação para aurora

---
+++ aurora/falas.py
@@ -0,0 +1,18 @@
+frases = [
+    "Ola", "Oi", "Opa", "E ai", "Olá", "Bom dia", "Boa tarde",
+    "Boa noite", "Como voce esta?", "Tudo bem?", "Como vai você?",
+    
+    "Me fale sobre redes neurais.", "O que é Deep Learning?", "Como funciona o processamento de linguagem natural?",
+    
+    "tchau", "ate logo", "flw",
+   
+]
+
+rotulos = [
+    "saudacao", "saudacao", "saudacao", "saudacao", "saudacao",
+    "saudacao", "saudacao", "saudacao", "saudacao", "saudacao", "saudacao",
+
+    "pergunta_tecnica","pergunta_tecnica", "pergunta_tecnica",
+
+    "despedida", "despedida","despedida",
+]
