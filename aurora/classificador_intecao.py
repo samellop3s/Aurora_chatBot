0a9d7d67c7a39cf4c181dc6bd4399cb5ef82d137
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-09-22 15:21:22 -0300

    feat : criação do vetorizador fixo

--- aurora/classificador_intecao.py
+++ aurora/classificador_intecao.py
@@ -1,4 +1,13 @@
+from sklearn.feature_extraction.text import CountVectorizer;
+from sklearn.neural_network import MLPClassifier
+
 #estamos indicando ao sistema a criação de uma "caixa" inteligente que vai receber as frases
 class ClassificadorIntecao:
     def __init__(self):
+        self.vetorizador = CountVectorizer()
+        self.modelo = MLPClassifier(
+            hidden_layer_sizes=(16,),
+            max_iter=3000, 
+            random_state=42,
+        )
         pass #estamos indicando que por enquanto nao existe informações para serem trabalhadas
