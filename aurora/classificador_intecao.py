409f736e4eab9fe51c3efe2fff27f8f887f32969
Samuel Ferreira Lopes <samuelferreiralopes6@gmail.com>
2026-09-22 14:57:09 -0300

    feat : identificando a função de cada linha descrita na class

--- aurora/classificador_intecao.py
+++ aurora/classificador_intecao.py
@@ -1,3 +1,4 @@
+#estamos indicando ao sistema a criação de uma "caixa" inteligente que vai receber as frases
 class ClassificadorIntecao:
     def __init__(self):
-        pass
+        pass #estamos indicando que por enquanto nao existe informações para serem trabalhadas
